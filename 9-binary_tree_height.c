f53ce9222f52d21cdb5c92bd42d84d62fce3a32f
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-02-26 07:18:13 -0800

    #10 task; height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - returns the height of a tree
+ * @tree: pointer to the root of the tree
+ * Return: returns int of height;
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+	return 1 + (left_height > right_height ? left_height : right_height);
+}

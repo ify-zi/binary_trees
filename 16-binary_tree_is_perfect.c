9d7c071a0f44f65479425518c4fff793a1331ffa
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-02-27 15:17:46 -0800

    perfect tree

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - check if a tree is perfect
+ * @tree: pointer to tree
+ * Return: returns 1 or zero
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
